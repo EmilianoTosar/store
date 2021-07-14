fec1b5a6d2f00f06f392774d33aa299c69eba4d8
Emiliano Tosar <emiliano.r.tosar@gmail.com>
2021-07-14 12:42:42 -0300

    Refactor functions

--- product/selectors.ts
+++ product/selectors.ts
@@ -1,28 +1,35 @@
-import { CartItem, Product } from "./types"
+import {CartItem, Product} from "./types";
 
-export function editCart(product: Product, action: 'decrement' | 'increment') {
-  return (cart: CartItem[]): CartItem[] => {  
-    const isInCart = cart.some(item => item.id === product.id)
+export function editCart(product: Product, action: "increment" | "decrement") {
+  return (cart: CartItem[]): CartItem[] => {
+    const isInCart = cart.some((item) => item.id === product.id);
 
-    if(!isInCart) {
-      return cart.concat({...product, quantity: 1})
+    if (!isInCart) {
+      return cart.concat({...product, quantity: 1});
     }
 
     return cart.reduce((acc, _product) => {
       if (product.id !== _product.id) {
-        return acc.concat(_product)
+        return acc.concat(_product);
       }
 
-      if(action === 'decrement') {
-        if (_product.quantity === 1) {
-          return acc
+      switch (action) {
+        case "decrement": {
+          if (_product.quantity === 1) {
+            return acc;
+          }
+
+          return acc.concat({..._product, quantity: _product.quantity - 1});
         }
-        
-        return acc.concat({..._product, quantity: _product.quantity - 1})
-      } else if (action === 'increment') {
-        return acc.concat({..._product, quantity: _product.quantity + 1})
+
+        case "increment": {
+          return acc.concat({..._product, quantity: _product.quantity + 1});
+        }
+
+        default: {
+          return acc.concat(_product);
+        }
       }
-    }, [])
-  }
-}  
- 
+    }, []);
+  };
+}
